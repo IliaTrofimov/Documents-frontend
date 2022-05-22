7709569280774838cffd98f2c9e497fff4d39020
Ilia Trofimov <ilia.trofimov2001@icloud.com>
2022-05-22 18:36:02 +0300

    improve templates list view

--- src/app/templates/templates-list/templates-list.component.ts
+++ src/app/templates/templates-list/templates-list.component.ts
@@ -1,10 +1,10 @@
 import { Router } from '@angular/router';
 import { Component, Input, OnInit } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { Template } from '../../models/template';
 import { TemplatesService } from '../../services/templates.service';
 import { DocumentsService } from '../../services/documents.service';
+import { AlertService } from 'src/app/services/alert.service';
 
 
 @Component({
@@ -19,46 +19,29 @@ export class TemplatesListComponent implements OnInit {
   constructor(private templateSvc: TemplatesService, 
     private documetnsSvc: DocumentsService,
     private router: Router,
-    private messageBar: MatSnackBar) {
+    private alertSvc: AlertService) {
   } 
 
-  showError(error: any){
-    this.messageBar.open(`Ошибка!\n${JSON.stringify(error)}`, "Принять", {
-      duration: 5000     
-    }); 
-  }
 
   ngOnInit(): void {
     if (!this.templates){
-      this.templateSvc.getTemplates().subscribe({
-        next: templates => {
-          this.templates = templates;
-        }
-      });
+      this.templateSvc.getTemplates().subscribe(templates => this.templates = templates);
     } 
   }
   
   addTemplate() {
-    this.templateSvc.createTemplate().subscribe({
-      next: templateId => this.router.navigate(["templates/" + templateId]),
-      error: err => this.showError(err)
-    });
+    this.templateSvc.createTemplate().subscribe(templateId => this.router.navigate(["templates/" + templateId]));
   }
 
   removeTemplate(id: number) {
-    this.templateSvc.deleteTemplate(id).subscribe({
-      next: () => {
-        if (this.templates) 
-          this.templates = this.templates.filter(template => template.Id !== id)
-      },
-      error: err => this.showError(err)
+    this.templateSvc.deleteTemplate(id).subscribe(() => {
+      this.alertSvc.info("Шаблон удалён")
+      if (this.templates) 
+        this.templates = this.templates.filter(template => template.Id !== id)
     });
   }
 
   createDocument(templateId: number) {
-    this.documetnsSvc.createDocument(templateId).subscribe({
-      next: id => this.router.navigate(["documents/" + id]),
-      error: err => this.showError(err)
-    });
+    this.documetnsSvc.createDocument(templateId).subscribe(id => this.router.navigate(["documents/" + id]));
   }
 }
