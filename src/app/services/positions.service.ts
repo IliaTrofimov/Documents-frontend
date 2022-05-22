e4e70219a2a0c4a00ecee4debdecc32688f638f5
Ilia Trofimov <ilia.trofimov2001@icloud.com>
2022-05-22 18:46:00 +0300

    updated positions svc

--- src/app/services/positions.service.ts
+++ src/app/services/positions.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 import { Position } from "../models/position";
 import { AppConfig } from '../app.config';
+import { SiteErrorCodes } from '../models/site-error';
+import { catchError, throwError } from 'rxjs';
+import { AlertService } from './alert.service';
 
 
 @Injectable()
 export class PositionsService{
     private url = "";
     
-    constructor(private http: HttpClient, private config: AppConfig){
+    constructor(private http: HttpClient, private config: AppConfig, private alertSvc: AlertService){
         this.url = this.config.apiUrl + "/positions";
     }
     
@@ -18,14 +21,52 @@ export class PositionsService{
     }
 
     updatePosition(pos: Position) {
-        return this.http.put(`${this.url}/${pos.Id}/put`, pos);
+        return this.http.put(`${this.url}/${pos.Id}/put`, pos).pipe(
+            catchError((error) => {
+                if (error instanceof HttpErrorResponse){
+                    switch (error.status){
+                        case SiteErrorCodes.NotFound: 
+                            this.alertSvc.error("Не удалось изменить должность", {message: "Данные не найдены."}); 
+                            break;
+                        default: 
+                            this.alertSvc.error("Не удалось изменить должность", {message: JSON.stringify(error.error, null, 2)}); 
+                            break;
+                    }
+                }
+                return throwError(() => new Error(error.message))
+            })
+        ); 
     }
 
     createPosition(pos: Position) {
-        return this.http.post<number>(`${this.url}/post`, pos);
+        return this.http.post<number>(`${this.url}/post`, pos).pipe(
+            catchError((error) => {
+                if (error instanceof HttpErrorResponse){
+                    this.alertSvc.error("Не удалось создать должность", {message: JSON.stringify(error.error, null, 2)}); 
+                }
+                return throwError(() => new Error(error.message))
+            })
+        ); 
     }
 
     deletePosition(id: number){
-        return this.http.delete(`${this.url}/${id}/delete`);
+        return this.http.delete(`${this.url}/${id}/delete`).pipe(
+            catchError((error) => {
+                if (error instanceof HttpErrorResponse){
+                    switch (error.status){
+                        case SiteErrorCodes.NotFound: 
+                            this.alertSvc.error("Не удалось удалить должность", {message: "Данные уже удалены."}); 
+                            break;
+                        case SiteErrorCodes.Conflict: 
+                            this.alertSvc.error("Не удалось удалить должность", {message: "Некоторые документы уже используют эти данные."}); 
+                            break;
+                        default: 
+                            this.alertSvc.error("Не удалось удалить должность", {message: JSON.stringify(error.error, null, 2)}); 
+                            break;
+                    }
+                }
+                return throwError(() => new Error(error.message))
+            })
+        ); 
     }
 }
