1d41e761af186849ca5d15bfab5991a3a93c4a6b
Ilia Trofimov <ilia.trofimov2001@icloud.com>
2022-05-22 18:53:00 +0300

    updated services

--- src/app/services/documents.service.ts
+++ src/app/services/documents.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 import { Document, DocumentStatus } from '../models/document';
 import { AppConfig } from '../app.config';
 import { DocumentDataItem } from '../models/document-data-item';
+import { catchError, throwError } from 'rxjs';
+import { SiteErrorCodes } from '../models/site-error';
+import { AlertService } from './alert.service';
 
 
 @Injectable()
 export class DocumentsService{
     private url = "";
     
-    constructor(private http: HttpClient, private config: AppConfig){
+    constructor(private http: HttpClient, private config: AppConfig, private alertSvc: AlertService){
         this.url = this.config.apiUrl + "/documents";
     }
     
@@ -24,12 +27,39 @@ export class DocumentsService{
     }
 
     updateDocument(document: Document){
-        return this.http.put(`${this.url}/${document.Id}/put`, document);
+        return this.http.put(`${this.url}/${document.Id}/put`, document).pipe(
+            catchError((error) => {
+                if (error instanceof HttpErrorResponse){
+                    switch (error.status){
+                        case SiteErrorCodes.NotFound: 
+                            this.alertSvc.error("Не удалось изменить документ", {message: "Данные не найдены."}); 
+                            break;
+                        default: 
+                            this.alertSvc.error("Не удалось изменить документ", {message: JSON.stringify(error.error, null, 2)}); 
+                            break;
+                    }
+                }
+                return throwError(() => new Error(error.message))
+            })
+        ); 
     }
 
     updateItem(docId: number, item: DocumentDataItem){
-        
-        return this.http.put(`${this.url}/${docId}/put-field`, item);
+        return this.http.put(`${this.url}/${docId}/put-field`, item).pipe(
+            catchError((error) => {
+                if (error instanceof HttpErrorResponse){
+                    switch (error.status){
+                        case SiteErrorCodes.NotFound: 
+                            this.alertSvc.error("Не удалось изменить данные", {message: "Данные не найдены."}); 
+                            break;
+                        default: 
+                            this.alertSvc.error("Не удалось изменить данные", {message: JSON.stringify(error.error, null, 2)}); 
+                            break;
+                    }
+                }
+                return throwError(() => new Error(error.message))
+            })
+        ); 
     }
 
     updateToTemplate(id: number){
@@ -42,10 +72,31 @@ export class DocumentsService{
             PreviousVersionId: previousVersionId,
             AuthorId: 0
         };
-        return this.http.post<number>(`${this.url}/post`, body);
+        return this.http.post<number>(`${this.url}/post`, body).pipe(
+            catchError((error) => {
+                if (error instanceof HttpErrorResponse){
+                    this.alertSvc.error("Не удалось создать документ", {message: JSON.stringify(error.error, null, 2)}); 
+                }
+                return throwError(() => new Error(error.message))
+            })
+        ); 
     }
 
     deleteDocument(id: number){
-        return this.http.delete(`${this.url}/${id}/delete`);
+        return this.http.delete(`${this.url}/${id}/delete`).pipe(
+            catchError((error) => {
+                if (error instanceof HttpErrorResponse){
+                    switch (error.status){
+                        case SiteErrorCodes.NotFound: 
+                            this.alertSvc.error("Не удалось удалить документ", {message: "Данные уже удалены."}); 
+                            break;
+                        default: 
+                            this.alertSvc.error("Не удалось удалить документ", {message: JSON.stringify(error.error, null, 2)}); 
+                            break;
+                    }
+                }
+                return throwError(() => new Error(error.message))
+            })
+        ); 
     }
 }
--- src/app/services/signatories.service.ts
+++ src/app/services/signatories.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 import { Signatory } from '../models/signatory';
 import { AppConfig } from '../app.config';
+import { catchError, throwError } from 'rxjs';
+import { SiteErrorCodes } from '../models/site-error';
+import { AlertService } from './alert.service';
 
 
 @Injectable()
 export class SignatoriesService{
     private url = "";
     
-    constructor(private http: HttpClient, private config: AppConfig){
+    constructor(private http: HttpClient, private config: AppConfig, private alertSvc: AlertService){
         this.url = this.config.apiUrl + "/signs";
     }
 
@@ -22,19 +25,75 @@ export class SignatoriesService{
     }
 
     getSign(userId: number, documentId: number){
-        return this.http.get<Signatory[]>(`${this.url}/get?documentId=${documentId}&userId=${userId}`);
+        return this.http.get<Signatory[]>(`${this.url}/get?documentId=${documentId}&userId=${userId}`).pipe(
+            catchError((error) => {
+                if (error instanceof HttpErrorResponse){
+                    switch (error.status){
+                        case SiteErrorCodes.NotFound: 
+                            this.alertSvc.error("Не удалось найти подпись", {message: "Данные не найдены."}); 
+                            break;
+                        default: 
+                            this.alertSvc.error("Не удалось найти подпись", {message: JSON.stringify(error.error, null, 2)}); 
+                            break;
+                    }
+                }
+                return throwError(() => new Error(error.message))
+            })
+        ); 
     }
 
     updateSign(sign: Signatory){
-        return this.http.put(`${this.url}/put`, JSON.stringify(sign));
+        return this.http.put(`${this.url}/put`, JSON.stringify(sign)).pipe(
+            catchError((error) => {
+                if (error instanceof HttpErrorResponse){
+                    switch (error.status){
+                        case SiteErrorCodes.NotFound: 
+                            this.alertSvc.error("Не удалось изменить подпись", {message: "Данные не найдены."}); 
+                            break;
+                        default: 
+                            this.alertSvc.error("Не удалось изменить подпись", {message: JSON.stringify(error.error, null, 2)}); 
+                            break;
+                    }
+                }
+                return throwError(() => new Error(error.message))
+            })
+        ); 
     }
 
     createSign(userId: number, documentId: number){
         const body = {userId: userId, documentId: documentId};
-        return this.http.post<number>(`${this.url}/post`, body);
+        return this.http.post<number>(`${this.url}/post`, body).pipe(
+            catchError((error) => {
+                if (error instanceof HttpErrorResponse){
+                    switch (error.status){
+                        case SiteErrorCodes.NotFound: 
+                            this.alertSvc.error("Не удалось создать подпись", {message: "Документ или подписант не найдены."}); 
+                            break;
+                        default: 
+                            this.alertSvc.error("Не удалось создать подпись", {message: JSON.stringify(error.error, null, 2)}); 
+                            break;
+                    }
+                }
+                return throwError(() => new Error(error.message))
+            })
+        ); 
     }
 
     deleteSign(id: number){
-        return this.http.delete(`${this.url}/${id}/delete`);
+        return this.http.delete(`${this.url}/${id}/delete`).pipe(
+            catchError((error) => {
+                if (error instanceof HttpErrorResponse){
+                    switch (error.status){
+                        case SiteErrorCodes.NotFound: 
+                            this.alertSvc.error("Не удалось удалить подпись", {message: "Данные не найдены."}); 
+                            break;
+                        default: 
+                            this.alertSvc.error("Не удалось удалить подпись", {message: JSON.stringify(error.error, null, 2)}); 
+                            break;
+                    }
+                }
+                return throwError(() => new Error(error.message))
+            })
+        ); 
     }
 }
