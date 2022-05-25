e1b6640616ebe84e830e88a4705650c938344e1b
Ilia Trofimov <ilia.trofimov2001@icloud.com>
2022-05-26 00:15:52 +0300

    changed navbar bg

--- src/app/shared-items/site-header.component.ts
+++ src/app/shared-items/site-header.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'site-header',
   template: `
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd; margin-bottom: 1em;">
       <a class="nav-link active" [routerLink]="['/home']" routerLinkActive="active">Домой</a>
       <a class="nav-link active" [routerLink]="['/templates']" routerLinkActive="active">Шаблоны</a>
       <a class="nav-link active" [routerLink]="['/documents']" routerLinkActive="active">Документы</a>
