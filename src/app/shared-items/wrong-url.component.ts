b49611d09b3c48243e749a74cf82e813bb6cd6c3
Ilia Trofimov <ilia.trofimov2001@icloud.com>
2022-06-13 21:03:03 +0300

    changed 404 page

--- src/app/shared-items/wrong-url.component.ts
+++ src/app/shared-items/wrong-url.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'error',
@@ -9,18 +7,10 @@ import { Subscription } from 'rxjs';
     <h4>Проверьте правильность адреса</h4>
     <hr>
     <p>
-      Вы пытались перейти по адресу <a [href]="url">{{url}}</a>, но данный адрес не существует или недоступен.<br>
-      Если вы уверены в правильности запроса, обратитесь к администратору.
+    Вы попали на несуществующую страницу. Вероятно, это произошло из-за опечатки или неправильной раскладки клавиатуры.<br>
+    Если вы уверены в правильности запроса, обратитесь к администратору.
     </p>
   `
 })
 export class WrongUrlComponent {
-  private routeSubscription?: Subscription;
-  url?: string;
-
-  constructor(private route: ActivatedRoute){
-    this.routeSubscription = route.params.subscribe(params => {
-      this.url = '/' + this.route.pathFromRoot.map(r => r.snapshot.url).filter(f => !!f[0]).map(([f]) => f.path).join('/');
-    });
-  }
 }
