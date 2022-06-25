62b79655803ab7b2e5edc1eb2e250fb7095b1d67
Ilia Trofimov <ilia.trofimov2001@icloud.com>
2022-06-25 18:15:48 +0300

    fixes

--- src/app/shared-items/debug.component.ts
+++ src/app/shared-items/debug.component.ts
@@ -4,15 +4,17 @@ import { AppConfig } from '../configurations/app.config';
 @Component({
   selector: 'debug',
   template: `
-    <small *ngIf="!prod" style="position:absolute;top:0;right: 0;font-family:Consolas;line-height:0.8;">
+    <small *ngIf="!prod" style="position:absolute;top:0;right: 0;font-family:Consolas;line-height:0.9;">
       api url <a target="_blank" rel="noopener noreferrer" href='{{url}}'>{{url}}</a><br>
       {{build}}<br>
+      {{startUp}}<br>
       {{userInfo}}
     </small>
   `,
 })
 export class DebugComponent implements OnInit {
   build: string = "not connected";
+  startUp: string = "start ";
   url: string = "";
   prod: boolean = false;
   userInfo: string = "";
@@ -24,10 +26,15 @@ export class DebugComponent implements OnInit {
     this.url = this.config.apiUrl;
     this.config.serverTest().subscribe(data => {
       this.build = data.BuildDate ? `build ${data.BuildDate}` : "not connected";
-      this.config.currentUser().subscribe(data => {
-        this.userInfo += data.Name ? `user '${data.Name}'` : "";
-        this.userInfo += data.AuthType ? ` auth-type '${data.AuthType}'` : "";
-      });
+      if (data.BuildDate){
+        this.startUp += data.StartupTime ? data.StartupTime : "unknown";
+        this.config.currentUser().subscribe(data => {
+          this.userInfo += data.Name ? `user '${data.Name}'` : "";
+          this.userInfo += data.AuthType ? ` auth-type '${data.AuthType}'` : "";
+          if (!this.userInfo) this.userInfo = "unauthenticated";
+        });
+      }
+      
     });
   }
 }
